2a003cbbc8a61d9d138d8c2096d9ab57d23d725b
김원정 <han909079@naver.com>
2020-08-06 17:44:21 +0900

    0806|어려워요쉬워요기능|develop|KWJ|S03P13A305-57

---
+++ backend/server/src/main/java/com/web/server/repo/HardEasyDaolmpl.java
@@ -0,0 +1,41 @@
+package com.web.server.repo;
+
+import com.web.server.dto.HardEasy;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.sql.SQLException;
+
+@Repository
+public class HardEasyDaolmpl implements HardEasyDao{
+    private String ns = "com.web.server.repo.HardEasyDao.";
+    @Autowired
+    SqlSessionTemplate template;
+
+
+    @Override
+    public void createHardChoice(HardEasy hardEasy) throws  SQLException{
+        template.insert(ns+"hardChoice",hardEasy);
+    }
+
+    @Override
+    public void createEasyChoice(HardEasy hardEasy) throws SQLException {
+        template.selectOne(ns+"EardChoice",hardEasy);
+    }
+
+    @Override
+    public void deleteChoice(HardEasy hardEasy) throws SQLException{
+        template.delete(ns+"deleteChoice",hardEasy);
+    }
+
+    @Override
+    public int updateHardChoice(Integer boardId) throws SQLException{
+        return template.update(ns+"updateHardChoice",boardId);
+    }
+
+    @Override
+    public int updateEasyChoice(Integer boardId) throws SQLException{
+        return template.update(ns+"updateEasyChoice",boardId);
+    }
+}
---
+++ backend/server/src/main/java/com/web/server/service/HardEasyService.java
@@ -0,0 +1,14 @@
+package com.web.server.service;
+
+import com.web.server.dto.HardEasy;
+
+import java.sql.SQLException;
+
+public interface HardEasyService {
+
+     void createHardChoice(HardEasy hardEasy) throws SQLException;
+
+     void createEasyChoice(HardEasy hardEasy) throws SQLException;
+
+     void deleteChoice(HardEasy hardEasy) throws SQLException;
+}
